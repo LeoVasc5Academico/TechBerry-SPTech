f9eff91d01b0b6b5f4099e3b0eb8bc76c05aa20f
= <=>
2022-04-18 18:18:07 -0300

    Upando arquivo para a correção da professora

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -64,11 +64,17 @@ function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
+    var cnpj = req.body.cnpjServer;
+    var telefone = req.body.telefoneServer;
     var senha = req.body.senhaServer;
 
     // Faça as validações dos valores
     if (nome == undefined) {
         res.status(400).send("Seu nome está undefined!");
+    }  else if (cnpj == undefined) {
+        res.status(400).send("Seu CNPJ está undefined!");
+    } else if (telefone == undefined) {
+        res.status(400).send("Seu telefone está undefined!");
     } else if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
@@ -76,7 +82,7 @@ function cadastrar(req, res) {
     } else {
         
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha)
+        usuarioModel.cadastrar(nome, cnpj, telefone, email, senha)
             .then(
                 function (resultado) {
                     res.json(resultado);
